eb011f1e70f4feb311ea1a3bdf55f31d0c8ccf4d
Mlezitom <mlezivat@gmail.com>
2023-07-10 13:38:50 +0200

    php 8.0

--- src/Translator.php
+++ src/Translator.php
@@ -5,54 +5,55 @@ namespace Mlezitom;
 /**
  * Translator
  *
- * @version 1.1
  * @author Tomas Mleziva
  */
-class Translator implements \Nette\Localization\ITranslator {
-    
+class Translator implements \Nette\Localization\Translator
+{
+
     public $data = array();
-    
-    function __construct() {
-		if(!class_exists("\Spyc")) {
-			require_once(dirname(__FILE__) . '/spyc.php');
-		}
+
+    function __construct()
+    {
+        if (!class_exists("\Spyc")) {
+            require_once(dirname(__FILE__) . '/spyc.php');
+        }
     }
 
-	public function translate($message, ...$parameters): string
-	{
-		if(array_key_exists($message, $this->data)) {
-			$translation = $this->data[$message];
-		}
-		else {
-			$translation = $message;
-		}
+    public function translate($message, ...$parameters): string
+    {
+        if (array_key_exists($message, $this->data)) {
+            $translation = $this->data[$message];
+        } else {
+            $translation = $message;
+        }
 
-		if(!count($parameters)) {
-			return (string)$translation;
-		}
+        if (!count($parameters)) {
+            return (string)$translation;
+        }
 
-		$sprintfParams = [$translation];
-		foreach($parameters as $parameter) {
-			$sprintfParams[] = $parameter;
-		}
-		return call_user_func_array("sprintf", $sprintfParams);
-	}
-    
-    public function loadFile($filename) {
-        if(!is_file($filename)) {
+        $sprintfParams = [$translation];
+        foreach ($parameters as $parameter) {
+            $sprintfParams[] = $parameter;
+        }
+        return call_user_func_array("sprintf", $sprintfParams);
+    }
+
+    public function loadFile($filename)
+    {
+        if (!is_file($filename)) {
             throw new \InvalidArgumentException("Translations file not found: " . $filename);
         }
-        
+
         $data = \Spyc::YAMLLoad($filename);
         $this->parseArrayRecursive($data);
     }
-    
-    private function parseArrayRecursive($node, $prefix = "") {
-        foreach($node as $key => $item) {
-            if(is_array($item)) {
+
+    private function parseArrayRecursive($node, $prefix = "")
+    {
+        foreach ($node as $key => $item) {
+            if (is_array($item)) {
                 $this->parseArrayRecursive($item, $prefix . "." . $key);
-            }
-            else {
+            } else {
                 $this->data[ltrim($prefix . "." . $key, ".")] = $item;
             }
         }
